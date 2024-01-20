da81c4e962536b6785eb5e23d72d03e6c42d6c17
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2024-01-20 16:01:47 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bus_tracking_application/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
